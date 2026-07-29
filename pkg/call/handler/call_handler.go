d65497fc9171576ac319240c4646bc4cc40020a3
RamonBritoDev <mrwexx07@gmail.com>
2026-07-29 13:37:27 +0000

    feat: add answer and hangup call HTTP handlers

--- pkg/call/handler/call_handler.go
+++ pkg/call/handler/call_handler.go
@@ -10,6 +10,8 @@ import (
 
 type CallHandler interface {
 	RejectCall(ctx *gin.Context)
+	AnswerCall(ctx *gin.Context)
+	HangupCall(ctx *gin.Context)
 }
 
 type callHandler struct {
@@ -43,6 +45,76 @@ func (g *callHandler) RejectCall(ctx *gin.Context) {
 	}
 
 	err = g.callService.RejectCall(data, instance)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{"message": "success"})
+}
+
+// Answer call
+// @Summary Answer call
+// @Description Answer an incoming call and (if it's a video call) accept its video
+// @Tags Call
+// @Accept json
+// @Produce json
+// @Param message body call_service.AnswerCallStruct true "Call data"
+// @Success 200 {object} gin.H "success"
+// @Failure 500 {object} gin.H "Internal server error"
+// @Router /call/answer [post]
+func (g *callHandler) AnswerCall(ctx *gin.Context) {
+	getInstance := ctx.MustGet("instance")
+
+	instance, ok := getInstance.(*instance_model.Instance)
+	if !ok {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "instance not found"})
+		return
+	}
+
+	var data *call_service.AnswerCallStruct
+	err := ctx.ShouldBindBodyWithJSON(&data)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	_, err = g.callService.AnswerCall(data, instance)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{"message": "success"})
+}
+
+// Hangup call
+// @Summary Hangup call
+// @Description Hangup an active call
+// @Tags Call
+// @Accept json
+// @Produce json
+// @Param message body call_service.HangupCallStruct true "Call data"
+// @Success 200 {object} gin.H "success"
+// @Failure 500 {object} gin.H "Internal server error"
+// @Router /call/hangup [post]
+func (g *callHandler) HangupCall(ctx *gin.Context) {
+	getInstance := ctx.MustGet("instance")
+
+	instance, ok := getInstance.(*instance_model.Instance)
+	if !ok {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "instance not found"})
+		return
+	}
+
+	var data *call_service.HangupCallStruct
+	err := ctx.ShouldBindBodyWithJSON(&data)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	err = g.callService.HangupCall(data, instance)
 	if err != nil {
 		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
